11633420caa80e5e80d2e17d443671dee0950a5b
neoki <ot07neoki@gmail.com>
2024-08-11 11:36:03 +0900

    refactor: remove language property from snippet interface

--- src/store.ts
+++ src/store.ts
@@ -4,7 +4,6 @@ interface Snippet {
   id: string;
   title: string;
   code: string;
-  language: string;
   author: string;
 }
 
@@ -13,21 +12,18 @@ const snippets: Snippet[] = [
     id: "1",
     title: "Snippet 1",
     code: `console.log('Hello, World!')`,
-    language: "javascript",
     author: "John Doe",
   },
   {
     id: "2",
     title: "Snippet 2",
     code: `print('Hello, World!')`,
-    language: "python",
     author: "Jane Doe",
   },
   {
     id: "3",
     title: "Snippet 3",
     code: `fmt.Println("Hello, World!")`,
-    language: "go",
     author: "John Doe",
   },
 ];
