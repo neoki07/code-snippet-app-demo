1010a2a08f4d31e3843242790c9c41822a76bdc8
neoki <ot07neoki@gmail.com>
2024-08-11 16:57:11 +0900

    fix: update get snippets function to order by descending snippet id

--- src/app/_db/get-snippets.ts
+++ src/app/_db/get-snippets.ts
@@ -1,6 +1,7 @@
 import { db } from "@/db/drizzle";
 import { snippet } from "@/db/schema";
+import { desc } from "drizzle-orm";
 
 export async function getSnippets() {
-  return db.select().from(snippet);
+  return db.select().from(snippet).orderBy(desc(snippet.id));
 }
