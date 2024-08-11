08392fb8d0ac4ad857cb1d51f860b9554d5dd9d0
neoki <ot07neoki@gmail.com>
2024-08-11 21:44:44 +0900

    feat: add authorization check in edit page

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,4 +1,23 @@
-export default function Page() {
+import { getSnippet } from "@/app/_db/get-snippet";
+import { currentUser } from "@clerk/nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  if (Number.isNaN(parseInt(params.id))) {
+    notFound();
+  }
+
+  const snippet = await getSnippet(parseInt(params.id));
+
+  if (!snippet) {
+    notFound();
+  }
+
+  const loggedInUser = await currentUser();
+  if (loggedInUser?.id !== snippet.authorId) {
+    redirect("/");
+  }
+
   return (
     <div>
       <h1 className="font-bold text-2xl inline">Edit Snippet</h1>
