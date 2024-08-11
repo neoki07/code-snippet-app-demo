b257c3f42dadecbd124e8e9f3b796b6064035f9d
neoki <ot07neoki@gmail.com>
2024-08-11 21:57:22 +0900

    refactor: add snippet form component and refactor new page component

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { SnippetForm } from "@/components/snippet-form";
 import { createSnippet } from "./actions";
 import { useFormState } from "react-dom";
 
@@ -11,7 +12,7 @@ const initialState = {
   },
 };
 export default function Page() {
-  const [state, formAction, pending] = useFormState(
+  const [state, formAction, isPending] = useFormState(
     createSnippet,
     initialState,
   );
@@ -19,56 +20,11 @@ export default function Page() {
   return (
     <div className="space-y-2">
       <h1 className="font-bold text-2xl inline">Create Snippet</h1>
-      <form action={formAction}>
-        <div className="space-y-3">
-          <div className="flex flex-col gap-0.5">
-            <label htmlFor="title" className="inline text-sm font-medium">
-              Title
-            </label>
-            <input
-              type="text"
-              id="title"
-              name="title"
-              className="border border-gray-300 rounded-md px-2 py-1.5 text-sm"
-            />
-            {state?.errors?.title?.map((error) => (
-              <p
-                key={error}
-                aria-live="polite"
-                className="text-red-500 text-sm"
-              >
-                {error}
-              </p>
-            ))}
-          </div>
-          <div className="flex flex-col gap-0.5">
-            <label htmlFor="code" className="inline text-sm font-medium">
-              Code
-            </label>
-            <textarea
-              id="code"
-              name="code"
-              rows={10}
-              className="border border-gray-300 rounded-md px-2 py-1.5 text-sm"
-            />
-            {state?.errors?.code?.map((error) => (
-              <p
-                key={error}
-                aria-live="polite"
-                className="text-red-500 text-sm"
-              >
-                {error}
-              </p>
-            ))}
-          </div>
-          <button
-            type="submit"
-            className="bg-black px-3 h-8 text-white rounded-md font-semibold text-sm"
-          >
-            {pending ? "Submitting..." : "Submit"}
-          </button>
-        </div>
-      </form>
+      <SnippetForm
+        action={formAction}
+        isPending={isPending}
+        errors={state.errors}
+      />
     </div>
   );
 }
---
+++ src/components/snippet-form.tsx
@@ -0,0 +1,66 @@
+interface SnippetFormProps {
+  action?: string | ((formData: FormData) => void);
+  isPending?: boolean;
+  errors?: {
+    title?: string[];
+    code?: string[];
+  };
+  defaultValues?: {
+    title?: string;
+    code?: string;
+  };
+}
+
+export function SnippetForm({
+  action,
+  isPending,
+  errors,
+  defaultValues,
+}: SnippetFormProps) {
+  return (
+    <form action={action}>
+      <div className="space-y-3">
+        <div className="flex flex-col gap-0.5">
+          <label htmlFor="title" className="inline text-sm font-medium">
+            Title
+          </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            defaultValue={defaultValues?.title}
+            className="border border-gray-300 rounded-md px-2 py-1.5 text-sm"
+          />
+          {errors?.title?.map((error) => (
+            <p key={error} aria-live="polite" className="text-red-500 text-sm">
+              {error}
+            </p>
+          ))}
+        </div>
+        <div className="flex flex-col gap-0.5">
+          <label htmlFor="code" className="inline text-sm font-medium">
+            Code
+          </label>
+          <textarea
+            id="code"
+            name="code"
+            defaultValue={defaultValues?.code}
+            rows={10}
+            className="border border-gray-300 rounded-md px-2 py-1.5 text-sm"
+          />
+          {errors?.code?.map((error) => (
+            <p key={error} aria-live="polite" className="text-red-500 text-sm">
+              {error}
+            </p>
+          ))}
+        </div>
+        <button
+          type="submit"
+          className="bg-black px-3 h-8 text-white rounded-md font-semibold text-sm"
+        >
+          {isPending ? "Submitting..." : "Submit"}
+        </button>
+      </div>
+    </form>
+  );
+}
